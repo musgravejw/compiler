9f36707d33cddefc290f9fcb8ee0030a2bab26d2
musgravejw <musgravejw@gmail.com>
2014-01-15 00:50:23 -0500

    Update scanner.rb

--- scanner.rb
+++ scanner.rb
@@ -6,65 +6,74 @@ class Scanner
   @line = 0
   @col = 0
   @filename = ""
-  @symbols = []
+  @symbols = []  
 
   def initialize(file)
     @line = 0
     @col = 0
     @filename = file
-    set_tokens()
+    set_symbols()
   end
 
   def get_next_token()
     if @filename.nil?
-	  # file not found
-	  puts "Fatal:  File not found.  Please enter a valid filename."
-	else	  
-	  file = File.open(@filename, 'r')  # open the file
-	  
-	  # move to the current line and column
-	  line = get_next_line(file)
-	  for i in 0..@col
-	  	line.getc
-	  end
+    # file not found
+    puts "Fatal:  File not found.  Please enter a valid filename."
+  else    
+    file = File.open(@filename, 'r')  # open the file
+    
+    @line.times{file.gets}  # move to the current line
 
-	  file.close()
-	end
+    row = file.gets
+    token = ""
+    i = 0    
+
+    row.each_char do |c|
+      if i >= @col
+        case c
+        when ' '          
+          @col = i + 1
+          break
+        when '\t'
+          @col = i + 1
+          break
+        when '\n'
+          @line += 1
+          @col = 0
+        else          
+          token += c.to_s
+        end
+      end
+      i += 1
+    end
+
+    puts token
+
+    # add token class
+    # if @sybols.include? token
+
+    # else
+
+    # end
+
+    file.close()
+  end
   end
 
   private
-    def get_next_line(file)
-      @line.times{file.gets}  # move to the current line
-      str = file.gets
-      @line += 1
-      return str
+    def set_symbols()
+      # define tokens
+    @symbols = ['=', ';', '(', ')', '{', '}', '>', '<', '>=', '<=', '==', '!=', '!', '&&', '||', '+', '-', '*', '/']    
     end
-
-  	def set_tokens()
-  	  # define tokens
-	  @symbols = ['=', ';', '(', ')', '{', '}', '<', '<=', '>', '>=',
-        '!', '!=', '==', '&&', '||', '+', '-', '*', '/']
-  	end
 end
 
 # create new file
 #File.open('test.rb', 'w') do |f|
-	# write  
+  # write  
 #end
-
-# get characters from the file
-#    while c = f.getc	  	
-#      if tokens.has_value? c
-#        current_token += c
-#      else
-#        while c != ' '
-#          current_token += c
-#          c = f.getc
-#        end	    	
-#      end
-#    end
 
 # get filename from command line argument
 file_in = ARGV[0]
 s = Scanner.new(file_in)
 puts s.get_next_token()
+
