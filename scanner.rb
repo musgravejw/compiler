cae118986c63a81db762ebee553625fa3ee724ef
musgravejw <musgravejw@gmail.com>
2014-01-14 21:23:19 -0500

    Update scanner.rb

--- scanner.rb
+++ scanner.rb
@@ -2,42 +2,69 @@
 # John Musgrave
 # Abstract:  This is the scanner to handle our lexical analysis
 
-# in x = x + y
+class Scanner
+  @line = 0
+  @col = 0
+  @filename = ""
+  @symbols = []
 
-# this is our hash of symbols
-tokens = {}
+  def initialize(file)
+    @line = 0
+    @col = 0
+    @filename = file
+    set_tokens()
+  end
 
-# define tokens
-tokens[:assign] = "="
-tokens[:semi_colon] = ";"
-tokens[:l_paren] = "("
-tokens[:r_paren] = ")"
+  def get_next_token()
+    if @filename.nil?
+	  # file not found
+	  puts "Fatal:  File not found.  Please enter a valid filename."
+	else	  
+	  file = File.open(@filename, 'r')  # open the file
+	  
+	  # move to the current line and column
+	  line = get_next_line(file)
+	  for i in 0..@col
+	  	line.getc
+	  end
+
+	  file.close()
+	end
+  end
+
+  private
+    def get_next_line(file)
+      @line.times{file.gets}  # move to the current line
+      str = file.gets
+      @line += 1
+      return str
+    end
+
+  	def set_tokens()
+  	  # define tokens
+	  @symbols = ['=', ';', '(', ')', '{', '}', '<', '<=', '>', '>=',
+        '!', '!=', '==', '&&', '||', '+', '-', '*', '/']
+  	end
+end
+
+# create new file
+#File.open('test.rb', 'w') do |f|
+	# write  
+#end
+
+# get characters from the file
+#    while c = f.getc	  	
+#      if tokens.has_value? c
+#        current_token += c
+#      else
+#        while c != ' '
+#          current_token += c
+#          c = f.getc
+#        end	    	
+#      end
+#    end
 
 # get filename from command line argument
 file_in = ARGV[0]
-
-if file_in.nil?
-	# file not found
-	puts "Fatal:  File not found.  Please enter a valid filename."
-else
-	# open the file
-	File.open(file_in, 'r') do |f|
-	  # get characters from the file
-	  while c = f.getc	  	
-	    # if tokens.has_value? c
-	    	# current_token += c
-	    # else
-	    	# while c != ' '
-	    		# current_token += c
-	    		# c = f.getc
-	    	# end	    	
-	    # end
-	  end
-	end
-end
-  
-# create new file
-# File.open('test.rb', 'w') do |f|
-	# write
-  
-#end
+s = Scanner.new(file_in)
+puts s.get_next_token()
