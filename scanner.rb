258154decbb4ea28c62f822d58ab491470c35a78
musgravejw <musgravejw@gmail.com>
2014-01-15 12:33:30 -0500

    Update scanner.rb

--- scanner.rb
+++ scanner.rb
@@ -17,64 +17,69 @@ class Scanner
 
   def get_next_token()
     if @filename.nil?
-    # file not found
-    puts "Fatal:  File not found.  Please enter a valid filename."
-  else    
-    file = File.open(@filename, 'r')  # open the file
-    
-    @line.times{file.gets}  # move to the current line
+      # file not found
+      puts "Fatal:  File not found.  Please enter a valid filename."
+    else
+      token = {}
+      lexeme = ""
+      i = 0
+      file = File.open(@filename, 'r')  # open the file    
+      @line.times{file.gets}  # move to the current line
+      row = file.gets      
 
-    row = file.gets
-    lexeme = ""
-    i = 0
-
-    # if we are at the end of line
-    if @col >= row.size
-    	row = file.gets
-    	@line += 1
-    	@col = 0
-    end
-
-    # look at each char
-    row.each_char do |c|      
-      # ignore if we've checked it before
-      if i >= @col      	
-        case c
-        when " "         
-          @col = i + 1
-          break
-        when "\t"
-          @col = i + 1
-          break
-        when "\n"
-          @line += 1
-          @col = 0
-          break        
-        else          
-          @col += 1
-          lexeme += c          
-        end
+      # if we are at the end of line
+      if @col >= row.size
+        row = file.gets
+        @line += 1
+        @col = 0
       end
-      i += 1
-    end    
 
-    puts lexeme
-
-    # add token class
-    # if @sybols.include? lexeme
-
-    # else
-
-    # end
-
-    file.close()
-  end
+      # look at each char
+      row.each_char do |c|      
+        # ignore if we've checked it before
+        if i >= @col      	
+          case c
+          when " "         
+            @col = i + 1
+            break
+          when "\t"
+            @col = i + 1
+            break
+          when "\n"
+            @line += 1
+            @col = 0
+            break        
+          else          
+            @col += 1
+            lexeme += c          
+          end
+        end
+        i += 1
+      end
+      token = create_token(lexeme)
+      file.close()
+    end
   end
 
   private
+
+    def create_token(lexeme)
+      token = {}
+      # check the token class
+      if lexeme.is_number?
+        token['class'] = "integer"
+      elsif @sybols.include? lexeme
+      	token['class'] = "operator"
+      else
+      	token['class'] = "identifier"
+      end
+      token['lexeme'] = lexeme
+      return token
+    end
+
     def set_symbols()
       # define tokens
-    @symbols = ['=', ';', '(', ')', '{', '}', '>', '<', '>=', '<=', '==', '!=', '!', '&&', '||', '+', '-', '*', '/']    
+      @symbols = ['=', ';', '(', ')', '{', '}', '>', '<', '>=', '<=', '==', '!=', '!', '&&', '||', '+', '-', '*', '/']    
     end
 end
 
