5230b7823c2415d1f07065606f58daac9ffd206f
musgravejw <musgravejw@gmail.com>
2014-01-17 22:55:15 -0500

    Create compiler.rb

---
+++ compiler.rb
@@ -0,0 +1,33 @@
+# compiler.rb
+# Written by John Musgrave
+# Main compiler runtime
+
+class Compiler
+  @line = 0
+  @error = ""
+  @scanner = nil
+
+  def initialize(filename)
+    @scanner = Scanner.new(filename)
+    start()
+  end
+
+  def start()
+  	token = ""
+  	while token != "EOF"
+      token = @scanner.get_next_token()
+    end
+  end
+
+  def reportError(message)
+    puts message
+  end
+
+  def reportWarning(message)
+    puts message
+  end
+end
+
+# get filename from command line arg
+file_in = ARGV[0]
+compiler = Compiler.new(file_in)
