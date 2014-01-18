cf926a339da58f598c64ea7c7bc7e1905117e763
musgravejw <musgravejw@gmail.com>
2014-01-17 22:55:01 -0500

    Update scanner.rb

--- scanner.rb
+++ scanner.rb
@@ -6,7 +6,10 @@
 class Scanner
   @line = 0
   @col = 0
+  @error = ""  
   @filename = ""
+  @symbol_table = []
+
   @whitespace = []
   @operators = []
   @keywords = []
@@ -14,10 +17,15 @@ class Scanner
   @right_paren = ""
   @semi_colon = ""
   @assignment = ""
+  @colon = ""
+  @comma = ""
+  @colon_equals = ""
+  @reserved_words = []
 
   def initialize(file)
     @line = 0
     @col = 0
+    @error = ""
     @filename = file
     set_symbols()
   end
@@ -52,6 +60,8 @@ class Scanner
           when "\t"
             @col = i + 1
             break
+          when "/" && row[i-1] == "/"
+            @col += 1
           when "\n"
             @line += 1
             @col = 0
@@ -74,10 +84,14 @@ class Scanner
     def create_token(lexeme)
       token = {}
       # check the token class      
-      if lexeme.is_number?
+      if is_numeric?(lexeme)
         token['class'] = "integer"
+      elsif is_string?(lexeme)
+        token['class'] = "string"
       elsif @operators.include? lexeme
         token['class'] = "operator"
+      elsif @reserved_words.include? lexeme
+        token['class'] = "reserved_word"
       elsif @keywords.include? lexeme
         token['class'] = "keyword"
       elsif lexeme == @left_paren
@@ -88,22 +102,47 @@ class Scanner
         token['class'] = "semi_colon"
       elsif lexeme == @assignment
         token['class'] = "assignment"
+      elsif lexeme == @colon
+        token['class'] = "colon"
+      elsif lexeme == @comma
+        token['class'] = "comma"
+      elsif lexeme == @colon_equals
+        token['class'] = "colon_equals"      
       else
-        token['class'] = "identifier"
+        token['class'] = "identifier" unless !is_identifier?(lexeme)
       end
       token['lexeme'] = lexeme
+      @symbol_table.push(token)
       return token
+    end
+
+    def is_identifier?(str)
+      return (str[/[a-zA-Z][a-zA-Z0-9_]*/]).size > 0
+    end
+
+    def is_string?(str)
+      return (str[/"[a-zA-Z0-9 _,;:.']*"/]).size > 0
+    end
+
+    def is_numeric?(str)
+      return (str[/[0-9][0-9_]*[.[0-9_]*]?/]).size > 0
     end
 
     def set_symbols()
       # define token class members
-        @whitespace = [" ", "\n", "\t", "\r"]
-        @operators = ["+", "-", "/", "*", "==", "!=", "!", "&&", "||"]
-        @keywords = []
-        @left_paren = "("
-        @right_paren = ")"
-        @semi_colon = ";"
-        @assignment = "="  
+      @whitespace = [" ", "\n", "\t", "\r"]
+      @operators = ["+", "-", "/", "*", "==", "!=", "!", "&&", "||"]      
+      @keywords = []
+      @left_paren = "("
+      @right_paren = ")"
+      @left_brace = "{"
+      @right_brace = "}"
+      @semi_colon = ";"
+      @assignment = "="
+      @colon = ":"
+      @comma = ","
+      @colon_equals = ":="
+      @reserved_words = ["string", "case", "int", "for", "bool", "and", "float", "or", "global", "not", "in", "program", "out", "procedure", "if", "begin", "then", "return", "else", "end", "EOF"]
     end
 end
 
@@ -111,8 +150,3 @@ end
 #File.open('test.rb', 'w') do |f|
   # write  
 #end
-
-# get filename from command line argument
-file_in = ARGV[0]
-s = Scanner.new(file_in)
-puts s.get_next_token()
