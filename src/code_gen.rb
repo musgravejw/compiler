7afdc08f5605cb4eec7c9fea5b43303f6001fa46
John Musgrave <musgravejw@gmail.com>
2014-04-17 14:26:44 -0400

    code generation

--- src/code_gen.rb
+++ src/code_gen.rb
@@ -5,6 +5,20 @@
 # Abstract
 #   Class for code generation
 
+# c := a + b;
+# d : = a + c + b;
+#
+# ;c := a + b;
+# R[1] = MM[44]; assumes variable a is at location 44
+# R[2] = MM[56];
+# R[1] = R[1] + R[2];
+# MM[32] = R[1];
+# ;d : = a + c + b;
+# R[1] = MM[44];
+# R[2] = MM[68];
+# R[1] = R[1] + R[2];
+# MM[44] = R[1];
+
 class CodeGen
   def initialize
   end
